9b37219c3657cd997100d734604d4fd4ff5c3721
samcamposr <samcamposr@outlook.com>
2019-05-10 19:28:43 -0300

    Mudando que banco de dados

--- ComputerAid/computeraid/settings.py
+++ ComputerAid/computeraid/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'computeraid.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'PDSI',
-        'USER': 'postgres',
-        'PASSWORD': '@computeraid',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
